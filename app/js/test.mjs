b17073fc81c5596e91a03c4320066a9b623ed1a9
mworth <mikeworthen55@gmail.com>
2020-12-22 14:38:30 -0700

    card search is working on test.html page

--- app/js/test.mjs
+++ app/js/test.mjs
@@ -1,34 +1,77 @@
-// let queryURLTest = "https://api.pokemontcg.io/v1/cards?name=";
-// console.log(queryURLTest)
-
-// axios
-//     .get(queryURLTest)
-//     .then(function (response) {
-//         let pokemon = response.data.sets
-//         let newResult = []
-
-
-//         // Loop through results, get data we want and push to new array
-//         for (let i = 0; i < pokemon.length; i++) {
-
-//             let targetResult = {
-//                 logo: pokemon[i].logoUrl,
-//                 number: pokemon[i].name,
-//                 release: pokemon[i].releaseDate,
-//                 symbol: pokemon[i].symbolUrl,
-
-//             }
-//             newResult.push(targetResult);
-//         }
+let butID = []
 
 $(document).ready(function() { 
   
     $("button").click(function() { 
-        
-      //here the value is stored in variable.  
+         
         var x = $("input:text").val();  
-
-        document.getElementById("demo").innerHTML = x; 
+        console.log(x);
+        butID.push(x);
+        cardSearch();
     }); 
 
 }); 
+
+function cardSearch(e) {
+
+let queryURLSearch = "https://api.pokemontcg.io/v1/cards?name=" + butID[0];
+console.log(queryURLSearch)
+
+axios
+    .get(queryURLSearch)
+    .then(function (response) {
+        let pokemon = response.data.cards
+        console.log(pokemon)
+        let newResult = []
+
+
+        // Loop through results, get data we want and push to new array
+        for (let i = 0; i < pokemon.length; i++) {
+
+            let targetResult = {
+                name: pokemon[i].name,
+                symbol: pokemon[i].symbolUrl,
+                imageUrl: pokemon[i].imageUrlHiRes,
+
+            }
+            newResult.push(targetResult);
+        }
+ 
+
+    for (let j = 0; j < newResult.length; j++) {
+
+        // let name = newResult[j].name;
+        let imageUrl = newResult[j].imageUrl;
+        // let symbol = newResult[j].symbol;
+
+        let aDiv1 = document.createElement('div');
+        // let setDiv = document.createElement('div');
+        // let setImage = document.createElement('div');
+        let logoImg = document.createElement('img');
+        // let setContent = document.createElement('div');
+        // let setNameDiv = document.createElement('div');
+        // let symbolImg = document.createElement('img');
+
+        logoImg.setAttribute('src', imageUrl);
+        logoImg.classList.add('testImg');
+        aDiv1.classList.add('test');
+        aDiv1.id = "testCard";
+        // aDiv1.setAttribute('name', "button");
+        // setImage.classList.add('card-image');
+        // setContent.classList.add('card-content');
+        // symbolImg.classList.add('symbolImg');
+
+        aDiv1.appendChild(logoImg);
+        // setDiv.appendChild(setImage);
+        // setImage.appendChild(logoImg);
+        // setDiv.appendChild(setContent);
+        // setContent.appendChild(setNameDiv);
+
+
+        document.getElementById('demo').append(aDiv1);
+
+    }
+
+});
+
+}
